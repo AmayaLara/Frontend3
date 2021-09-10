509948193108eea78a5a38bfdc701f36db285b6b
AmayaLara <81272948+AmayaLara@users.noreply.github.com>
2021-09-10 15:41:46 -0300

    segundo commit examen front

--- src/App.js
+++ src/App.js
@@ -1,69 +1,80 @@
 import React from "react";
-import data from "./components/data.json"
-import Botones from "./components/botones"
-import Recordatorio from "./components/recordatorio"
+import data from "./components/data.json";
+import Botones from "./components/botones";
+import Recordatorio from "./components/recordatorio";
 
 let i = 1;
 
 export default class App extends React.Component {
-  constructor(){
+  constructor() {
     super();
-    this.state= {
+    this.state = {
       contador: 0,
       arrayData: [],
       historia: data[0].historia,
       opcionA: data[0].opciones.a,
       opcionB: data[0].opciones.b,
       opcionAnterior: "",
-      opcionesSeleccionadas: [],
-    }  
+      opcionesSeleccionadas: []
+    };
   }
 
-  componentDidMount(){
-    this.setState({ arrayData: data})
+  componentDidMount() {
+    this.setState({ arrayData: data });
   }
 
   handleChange = (opcion) => {
-    i++
-    let caminoElegido = data.filter(data => data.id === i + opcion)[0]
-    if(i === 6){
-      alert("FIN!")
-    }
-    else{
+    i++;
+    let caminoElegido = this.state.arrayData.filter(
+      (data) => data.id === i + opcion
+    )[0];
+    if (i === 6) {
+      alert("FIN!");
+    } else {
       this.setState((prevState) => ({
         historia: caminoElegido.historia,
         opcionA: caminoElegido.opciones.a,
         opcionB: caminoElegido.opciones.b,
         opcionAnterior: opcion.toUpperCase(),
-        opcionesSeleccionadas: [...prevState.opcionesSeleccionadas, opcion.toUpperCase()],
-      }))
+        opcionesSeleccionadas: [
+          ...prevState.opcionesSeleccionadas,
+          opcion.toUpperCase()
+        ]
+      }));
     }
-  }
+  };
 
   handleRestart = () => {
-    this.setState= {
+    this.setState = {
       arrayData: [],
       historia: data[0].historia,
       opcionA: data[0].opciones.a,
       opcionB: data[0].opciones.b,
       opcionAnterior: "",
-      opcionesSeleccionadas: [],
-    }
-  }
+      opcionesSeleccionadas: []
+    };
+  };
 
-  render(){
-    return( 
-    <div className="App">
-      <div className="layout">
-        <h1 className="historia">{this.state.historia}</h1>
-        <Botones recibirSeleccion ={this.handleChange}  opcionA={this.state.opcionA} opcionB={this.state.opcionB} />
-        <Recordatorio opcionAnterior= {this.state.opcionAnterior} opcionesSeleccionadas={this.state.opcionesSeleccionadas}/>
-        <br/>
-        <form type="submit" onClick={this.handleRestart} >
-          <button className="botones" > Reiniciar historia </button>
-        </form>
+  render() {
+    return (
+      <div className="App">
+        <div className="layout">
+          <h1 className="historia">{this.state.historia}</h1>
+          <Botones
+            recibirSeleccion={this.handleChange}
+            opcionA={this.state.opcionA}
+            opcionB={this.state.opcionB}
+          />
+          <Recordatorio
+            opcionAnterior={this.state.opcionAnterior}
+            opcionesSeleccionadas={this.state.opcionesSeleccionadas}
+          />
+          <br />
+          <form type="submit" onClick={this.handleRestart}>
+            <button className="botones"> Reiniciar historia </button>
+          </form>
+        </div>
       </div>
-    </div>
-    )}
-
+    );
+  }
 }
